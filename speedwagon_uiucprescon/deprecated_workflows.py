124a78e3ccb1b55034aee4e24d32eb3cdf5abf4b
Henry Borchers <henryborchers@yahoo.com>
2024-03-15 14:09:48 -0500

    refactor: pep8 spacing

--- speedwagon_uiucprescon/deprecated_workflows.py
+++ speedwagon_uiucprescon/deprecated_workflows.py
@@ -1,5 +1,7 @@
 import speedwagon
 from .plugin import deprecated_workflows
+
+
 @speedwagon.hookimpl
 def registered_workflows():
     return {workflow.name: workflow for workflow in deprecated_workflows}
