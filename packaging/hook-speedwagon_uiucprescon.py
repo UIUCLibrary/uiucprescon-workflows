6a9db0797ad8cde0511a165e6a8e0ac568b7a12e
Henry Borchers <henryborchers@yahoo.com>
2024-08-01 12:15:45 -0500

    remove unused scripts

--- packaging/hook-speedwagon_uiucprescon.py
+++
@@ -1,7 +0,0 @@
-from PyInstaller.utils.hooks import copy_metadata, collect_all
-datas, binaries, hiddenimports = collect_all('speedwagon_uiucprescon')
-datas += copy_metadata('speedwagon_uiucprescon', recursive=True)
-hiddenimports =[
-    'speedwagon_uiucprescon.active_workflows',
-    'speedwagon_uiucprescon.deprecated_workflows',
-    ]
